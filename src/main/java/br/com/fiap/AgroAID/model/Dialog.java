a187f295aeb491d2667a6915418052233ecd49a5
HenriqueFreitas02 <haf@hotmail.com.br>
2023-11-10 21:40:32 -0300

    chat done

--- src/main/java/br/com/fiap/AgroAID/model/Dialog.java
+++ src/main/java/br/com/fiap/AgroAID/model/Dialog.java
@@ -1,31 +1,23 @@
 package br.com.fiap.AgroAID.model;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
 @Data
-public class Chat {
+public class Dialog {
     
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
-    private String text;
+    private String title;
 
-    private Long dialogId;
 
-    private boolean bot;
-    private String time;
 }
